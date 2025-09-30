fa2ee6532265c99e10561b4988076af40529ce79
nazita04 <nazaabril04@gmail.com>
2025-09-30 14:51:36 -0300

    convertir Validators a metodos estaticos

--- abmBackend/Services/UsuarioServices.cs
+++ abmBackend/Services/UsuarioServices.cs
@@ -4,7 +4,6 @@ using abm.validators;
 
 namespace abm.Services
 {
-
     public class UsuarioServices
     {
         private readonly AppDbContext _context;
@@ -17,16 +16,15 @@ namespace abm.Services
         //metodo para crear nuevo usuario
         public async Task<Usuario> CrearUsuario(Usuario nuevoUsuario)
         {
-            Validators validator = new Validators();
 
-            validator.isValidNombreYApellido(nuevoUsuario.nombre, nuevoUsuario.apellido);
+            Validators.isValidNombreYApellido(nuevoUsuario.nombre, nuevoUsuario.apellido);
 
-            if (!validator.isValidEmail(nuevoUsuario.email))
+            if (!Validators.isValidEmail(nuevoUsuario.email))
             {
                 throw new ArgumentException("El formato de email ingresado no es valido");
             }
 
-            validator.isValidDni(nuevoUsuario.dni);
+            Validators.isValidDni(nuevoUsuario.dni);
 
             var usuarioExiste = (from user in _context.Usuarios
                                  where user.dni == nuevoUsuario.dni
@@ -37,7 +35,7 @@ namespace abm.Services
                 throw new InvalidOperationException("El Usuario ya está registrado");
             }
 
-            validator.isValidPassword(nuevoUsuario.password);
+            Validators.isValidPassword(nuevoUsuario.password);
 
             _context.Usuarios.Add(nuevoUsuario);
             await _context.SaveChangesAsync();
