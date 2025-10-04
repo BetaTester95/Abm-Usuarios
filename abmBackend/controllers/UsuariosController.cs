5d14f935875ae33fc069c1e31aaca4b4669a354d
nazita04 <nazaabril04@gmail.com>
2025-10-03 23:20:40 -0300

    Registar metodo POST en el controller

---
+++ abmBackend/controllers/UsuariosController.cs
@@ -0,0 +1,41 @@
+﻿using abm.models;
+using abm.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace abm.controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuariosController : ControllerBase
+    {
+        //agrego el servicio
+        private readonly UsuarioServices _usuarioServices;
+
+        public UsuariosController (UsuarioServices usuarioServices)
+        {
+            _usuarioServices = usuarioServices;
+        }
+
+        [HttpPost] //método post
+        public async Task<IActionResult> AgregarUsuario([FromBody] Usuario usuario ) //recibe un json en el cuerpo de la peticion (Postman)
+        {
+            try
+            {
+                var usuarioCreado = await _usuarioServices.CrearUsuario(usuario);
+                return Ok(usuarioCreado);
+
+            }
+            catch(SqlException e)
+            {
+                return StatusCode(503, new { mensaje = "Error de conexión a la base de datos" });
+            }
+            catch (Exception e) 
+            {
+                return StatusCode(500, new { mensaje = "Error interno del servidor" }); //error que me impide procesar la solicitud
+
+            }
+
+        }
+    }
+}
