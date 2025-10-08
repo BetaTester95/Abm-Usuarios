dd79ba1318d36948b27d024ece5a4df2437ae783
nazita04 <nazaabril04@gmail.com>
2025-10-07 22:31:23 -0300

    modificaciones en las validaciones

--- abmBackend/Program.cs
+++ abmBackend/Program.cs
@@ -1,10 +1,13 @@
 using abm.data;
 using abm.Services;
 using abm.validators;
+using abm.controllers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Win32;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -15,9 +18,11 @@ builder.Services.AddScoped<Validators>();
 // Registrar UsuarioServices
 builder.Services.AddScoped<UsuarioServices>();
 
-builder.Services.AddControllers();
-
 var app = builder.Build();
+
+//verificar que la api este corriendo
+app.MapGet("/", () => "¡La API está funcionando!");
+
 app.UseAuthentication();
 app.MapControllers();
 app.Run();
