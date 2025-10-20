35d05a27f99761c495337946a9ce66676f01c7e2
BetaTester95 <shuliaans@gmail.com>
2025-10-20 17:53:20 -0300

    #

---
+++ abmFrontEnd/main.js
@@ -0,0 +1,27 @@
+const listUsersContainer = document.getElementById('listUsers'); 
+
+
+async function listarUsuarios() {
+
+    const getUsers = 'http://localhost:5021/api/usuarios/obtenerUsuarios';
+
+    const response = await fetch(getUsers);
+    const users = await response.json()
+    console.log(users)
+
+    let tableBodyContent = '';
+    users.data.forEach(user => {
+        tableBodyContent += `
+                <tr>
+                    <th scope="row">${user.usuarioId}</th>
+                    <td>${user.nombre || 'N/A'}</td>
+                    <td>${user.apellido || 'N/A'}</td>
+                    <td>${user.email || 'No disponible'}</td>
+                    <td>${user.dni || 'No disponible'}</td>
+                </tr>
+            `;
+    });
+    listUsersContainer.innerHTML = tableBodyContent;
+
+}
+listarUsuarios()
